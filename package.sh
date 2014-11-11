b90d1d903c2cf75fe9530c14b282c3580042697a
Dan Garrette <dhgarrette@gmail.com>
2014-11-11 13:06:15 -0600

    added package.sh script

---
+++ package.sh
@@ -0,0 +1,3 @@
+sbt package;    mv target/scala-2.10/opennlp-maxent_2.10-3.0.3.jar         ../low-resource-pos-tagging-2014/lib/opennlp-maxent-3.0.3.jar
+sbt packageSrc; mv target/scala-2.10/opennlp-maxent_2.10-3.0.3-sources.jar ../low-resource-pos-tagging-2014/lib/opennlp-maxent-3.0.3-sources.jar
+
